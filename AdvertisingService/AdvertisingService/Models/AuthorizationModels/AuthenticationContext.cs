7cb686448b077191655ab0ea05c9ecac18c772a0
Eugene Trifonov <eugene_trifonov@bitbucket.org>
2019-09-02 18:03:36 +0300

    pre add some info to db

--- AdvertisingService/AdvertisingService/Models/AuthorizationModels/AuthenticationContext.cs
+++ AdvertisingService/AdvertisingService/Models/AuthorizationModels/AuthenticationContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdvertisingService.Models
@@ -7,9 +8,21 @@ namespace AdvertisingService.Models
     {
         public AuthenticationContext(DbContextOptions<AuthenticationContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole[]
+                {
+                new IdentityRole { Id="1", Name="Admin", NormalizedName="ADMIN"},
+                new IdentityRole { Id="2", Name="User", NormalizedName="USER"}
+                });
+        }
     }
 }
